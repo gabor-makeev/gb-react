f2c48c5be0acf90c2895ce7aa258858b460295ac
Gabor Makeev <gabor.makejev@gmail.com>
2022-06-02 01:16:58 +0200

    variant 2. tasks 8, 9 completed. set up the storybook to be published on gh-pages. added webhook for linter. updated webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,28 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const BundleAnalyzerPlugin =
+  require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 const isDev = process.env.NODE_ENV === 'development';
+const withReport = process.env.npm_config_withReport;
 
 module.exports = {
-  entry: path.resolve(__dirname, './src/index.jsx'),
-  output: {
-    clean: true,
-    environment: {
-      arrowFunction: false,
+  devServer: {
+    client: {
+      logging: 'info',
     },
-    filename: '[name].bundle.[chunkhash].js',
-    path: path.resolve(__dirname, './build'),
+    compress: true,
+    historyApiFallback: true,
+    port: 8000,
   },
-  resolve: {
-    extensions: ['.jsx', '.js'],
-  },
+  devtool:
+    process.env.NODE_ENV === 'production'
+      ? 'hidden-source-map'
+      : 'eval-source-map',
+  entry: path.resolve(__dirname, './src/index.jsx'),
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
   module: {
     rules: [
       {
@@ -57,7 +63,30 @@ module.exports = {
           'sass-loader',
         ],
       },
+      {
+        generator: {
+          filename: 'static/[hash][ext]',
+        },
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
     ],
+  },
+  optimization: {
+    minimizer: ['...', new CssMinimizerPlugin()],
+  },
+  output: {
+    clean: true,
+    environment: {
+      arrowFunction: false,
+    },
+    filename: '[name].bundle.[chunkhash].js',
+    path: path.resolve(__dirname, './build'),
+  },
+  performance: {
+    hints: false,
+    maxAssetSize: 512000,
+    maxEntrypointSize: 512000,
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -71,5 +100,15 @@ module.exports = {
             filename: '[name].[contenthash].css',
           }),
         ]),
+    ...(withReport ? new BundleAnalyzerPlugin() : ''),
   ],
+  resolve: {
+    alias: {
+      components: path.resolve(__dirname, 'src/components/'),
+      src: path.resolve(__dirname, 'src'),
+      store: path.resolve(__dirname, 'src/store'),
+      svg: path.resolve(__dirname, 'src/assets/svg'),
+    },
+    extensions: ['.jsx', '.js'],
+  },
 };
