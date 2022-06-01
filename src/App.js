37f1327ca92c142f34ddbba86fa6e7a5a2fe17e8
Gabor Makeev <gabor.makejev@gmail.com>
2022-06-01 20:14:09 +0200

    lesson 2 preparation. cleaned the project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import style from './App.module.scss'
-import {Message} from "./components/Message/Message"
-import {ContactList} from "./components/ContactList/ContactList";
-import {Header} from "./components/Header/Header";
+import style from './App.module.scss';
 
 function App() {
-  const messageContent = 'The content of a message'
-  const contacts = [
-    'John',
-    'Sam',
-    'Carl',
-    'Lisa'
-  ]
 
   return (
-    <div className={style.App}>
-      <Header />
-      <Message content={messageContent} />
-      <ContactList contacts={contacts}/>
-    </div>
+    <div className={style.App}></div>
   );
 }
 
