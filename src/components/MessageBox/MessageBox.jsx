439d9d327b385a159fff8ebff4ab76905901677a
Gabor Makeev <gabor.makejev@gmail.com>
2022-06-04 01:05:26 +0200

    removed unnecessary 'return' statement from MessageBox.jsx

--- src/components/MessageBox/MessageBox.jsx
+++ src/components/MessageBox/MessageBox.jsx
@@ -4,16 +4,14 @@ export const MessageBox = (props) => {
   return (
     <div className={style['message-box']}>
       <ul className={style['message-box__list']}>
-        {props.messageList.map((message, idx) => {
-          return (
-            <li className={style['message-box__item']} key={idx}>
-              {message.text}
-              <span className={style['message-box__item__author']}>
-                {message.author}
-              </span>
-            </li>
-          );
-        })}
+        {props.messageList.map((message, idx) => (
+          <li className={style['message-box__item']} key={idx}>
+            {message.text}
+            <span className={style['message-box__item__author']}>
+              {message.author}
+            </span>
+          </li>
+        ))}
       </ul>
     </div>
   );
