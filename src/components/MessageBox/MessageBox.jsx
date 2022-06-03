690a95be2d241070bf5090b03b22a18e3543208f
Gabor Makeev <gabor.makejev@gmail.com>
2022-06-04 01:13:50 +0200

    added component property destructuring assignment in MessageBox.jsx

--- src/components/MessageBox/MessageBox.jsx
+++ src/components/MessageBox/MessageBox.jsx
@@ -1,10 +1,10 @@
 import style from './MessageBox.module.scss';
 
-export const MessageBox = (props) => {
+export const MessageBox = ({ messageList }) => {
   return (
     <div className={style['message-box']}>
       <ul className={style['message-box__list']}>
-        {props.messageList.map((message, idx) => (
+        {messageList.map((message, idx) => (
           <li className={style['message-box__item']} key={idx}>
             {message.text}
             <span className={style['message-box__item__author']}>
