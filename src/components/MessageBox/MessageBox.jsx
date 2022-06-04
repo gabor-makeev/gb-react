64f940acb7565751f15fb87ddda9ad9648e22d79
Gabor Makeev <gabor.makejev@gmail.com>
2022-06-04 02:14:04 +0200

    added STYLES constant to constants.js. added 'primaryColor' and 'secondaryColor' properties to MessageBox, their values apply to .message-box and .message-box__item elements of the component. removed background-color style property of .message-box and .message-box__item elements from MessageBox.module.scss

--- src/components/MessageBox/MessageBox.jsx
+++ src/components/MessageBox/MessageBox.jsx
@@ -1,11 +1,28 @@
 import style from './MessageBox.module.scss';
+import { STYLES } from '../../constants';
 
-export const MessageBox = ({ messageList }) => {
+export const MessageBox = ({
+  messageList,
+  primaryColor = STYLES.color.primary,
+  secondaryColor = STYLES.color.secondary,
+}) => {
+  const messageBoxStyle = {
+    backgroundColor: primaryColor,
+  };
+
+  const messageBoxItemStyle = {
+    backgroundColor: secondaryColor,
+  };
+
   return (
-    <div className={style['message-box']}>
+    <div className={style['message-box']} style={messageBoxStyle}>
       <ul className={style['message-box__list']}>
         {messageList.map((message, idx) => (
-          <li className={style['message-box__item']} key={idx}>
+          <li
+            className={style['message-box__item']}
+            key={idx}
+            style={messageBoxItemStyle}
+          >
             {message.text}
             <span className={style['message-box__item__author']}>
               {message.author}
--- src/constants.js
+++ src/constants.js
@@ -2,3 +2,10 @@ export const AUTHORS = {
   user: 'Gabor',
   bot: '🤖 Bot',
 };
+
+export const STYLES = {
+  color: {
+    primary: '#FF4F4F',
+    secondary: '#31E865',
+  },
+};
