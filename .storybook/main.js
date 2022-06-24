e55256f5898da7a39c8640c469d6ae397a27db4d
Gabor Makeev <gabor.makejev@gmail.com>
2022-06-24 22:31:48 +0200

    adjustments: fixed alias issue in storybook ('build-storybook' script error while aliases are used). config adjustments were done in .storybook/main.js

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,4 @@
+const path = require("path");
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
@@ -12,5 +13,15 @@ module.exports = {
   "framework": "@storybook/react",
   "core": {
     "builder": "@storybook/builder-webpack5"
+  },
+  "webpackFinal": async (config) => {
+    config.resolve.alias = {
+      components: path.resolve(__dirname, '../src/components/'),
+      src: path.resolve(__dirname, '../src'),
+      store: path.resolve(__dirname, '../src/store'),
+      svg: path.resolve(__dirname, '../src/assets/svg'),
+    };
+
+    return config;
   }
 }
