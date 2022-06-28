f25c7b6142a6616e2f109c128fa30383e08f1387
Gabor Makeev <gabor.makejev@gmail.com>
2022-06-28 20:15:36 +0200

    task 1. part 1. installed redux toolkit. replaced createStore with configureStore in src/store/index.ts

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,19 @@
-import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
+import { combineReducers } from 'redux';
 import { profileReducer } from './profile/reducer';
 import { messagesReducer } from './messages/reducer';
 import storage from 'redux-persist/lib/storage';
 import { persistStore, persistReducer } from 'redux-persist';
 import createSagaMiddleware from 'redux-saga';
 import mySaga from './sagas';
-
-export const composeEnhancers =
-  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+import { configureStore } from '@reduxjs/toolkit';
+import {
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist/es/constants';
 
 export type StoreState = ReturnType<typeof rootReducer>;
 
@@ -25,10 +31,16 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const sagaMiddleware = createSagaMiddleware();
 
-export const store = createStore(
-  persistedReducer,
-  composeEnhancers(applyMiddleware(sagaMiddleware))
-);
+export const store = configureStore({
+  reducer: persistedReducer,
+  devTools: process.env.NODE_ENV !== 'production',
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(sagaMiddleware),
+});
 
 sagaMiddleware.run(mySaga);
 
