054dba28e3b953daef0fa660493d5d038d47fd38
Gabor Makeev <gabor.makejev@gmail.com>
2022-06-29 23:28:24 +0200

    task 1. part 6. added loading sign ('Loading') which is displayed while the articles data is being fetched. also an artificial delay of 1 second was added in order to display the loading sign. added 'Get articles' button which initializes the fetchArticles function. articles data is displayed in the layout only once the loading process is done

--- src/pages/Articles/Articles.tsx
+++ src/pages/Articles/Articles.tsx
@@ -4,15 +4,23 @@ import { Article } from 'src/default-types';
 
 export const Articles: FC = () => {
   const [articles, setArticles] = useState<Article[]>([]);
+  const [loading, setLoading] = useState(false);
   const [error, setError] = useState('');
 
   const fetchArticles = async () => {
+    setLoading(true);
+    setError('');
+
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
     try {
       const res = await fetch(api);
       const data = await res.json();
       setArticles(data);
     } catch (err) {
       setError((err as Error).message);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -23,12 +31,16 @@ export const Articles: FC = () => {
   return (
     <>
       <h2>Articles</h2>
+      <button onClick={fetchArticles}>Get articles</button>
+      {loading && <p>Loading</p>}
       {error && <p>Error: {error}</p>}
-      <ul>
-        {articles.map((article) => (
-          <li key={article.id}>{article.title}</li>
-        ))}
-      </ul>
+      {!loading && (
+        <ul>
+          {articles.map((article) => (
+            <li key={article.id}>{article.title}</li>
+          ))}
+        </ul>
+      )}
     </>
   );
 };
