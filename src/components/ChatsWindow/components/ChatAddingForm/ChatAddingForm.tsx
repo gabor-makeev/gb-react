cab24ebe9e31d78339fc86b390d72b3a4cfd5566
Gabor Makeev <gabor.makejev@gmail.com>
2022-07-01 01:37:11 +0200

    task 3. part 2. added stories for ChatAddingForm

--- src/components/ChatsWindow/components/ChatAddingForm/ChatAddingForm.tsx
+++ src/components/ChatsWindow/components/ChatAddingForm/ChatAddingForm.tsx
@@ -2,20 +2,29 @@ import React, { FC } from 'react';
 import TextField from '@mui/material/TextField';
 import { Button } from '@mui/material';
 import style from './ChatAddingForm.module.scss';
+import classNames from 'classnames';
 
 interface ChatAddingFormProps {
   onAddChat: (e: React.FormEvent<HTMLFormElement>, inputValue: string) => void;
   inputValue: string;
   setInputValue: (value: string) => void;
+  hoverable?: boolean;
+  disabledInput?: boolean;
+  disabledButton?: boolean;
 }
 
 export const ChatAddingForm: FC<ChatAddingFormProps> = ({
   onAddChat,
   inputValue,
   setInputValue,
+  hoverable = false,
+  disabledInput = false,
+  disabledButton = false,
 }) => {
+  const classes = classNames(style.form, hoverable ? style.hoverable : '');
+
   return (
-    <form onSubmit={(e) => onAddChat(e, inputValue)} className={style.form}>
+    <form onSubmit={(e) => onAddChat(e, inputValue)} className={classes}>
       <TextField
         label={'Chat name'}
         variant="outlined"
@@ -23,8 +32,13 @@ export const ChatAddingForm: FC<ChatAddingFormProps> = ({
         value={inputValue}
         data-testid={'ChatAddingFormTextField'}
         onChange={(e) => setInputValue(e.target.value)}
+        disabled={disabledInput}
       />
-      <Button data-testid={'ChatAddingFormButton'} type={'submit'}>
+      <Button
+        data-testid={'ChatAddingFormButton'}
+        type={'submit'}
+        disabled={disabledButton}
+      >
         Add chat
       </Button>
     </form>
