aa3f38f352477941e2c9cbfb3fb82aac544ff4d0
gabormakeev <gabor.makejev@gmail.com>
2022-07-03 01:53:22 +0300

    variant 1. task 1 completed. signed up for firebase. created 'gb-react' project and added 'gb-react' Web App to it (see firebase.PNG in the root of the project repository). installed firebase package. configured firebase connection in src/services/firebase.ts

---
+++ src/services/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAh-tV-H0TbqxVHCSqMPdBLwQivTl_1u5I',
+  authDomain: 'gb-react-ca669.firebaseapp.com',
+  projectId: 'gb-react-ca669',
+  storageBucket: 'gb-react-ca669.appspot.com',
+  messagingSenderId: '414948860769',
+  appId: '1:414948860769:web:184056e3963ddbfdd25b20',
+};
+
+const app = initializeApp(firebaseConfig);
