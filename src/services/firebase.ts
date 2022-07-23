ad9be69079482c9f717e517a205e2f96a22afa9d
Gabor Makeev <gabor.makejev@gmail.com>
2022-07-23 15:36:12 +0200

    refactor: change firebase realtime database connection initialization location to firebase.ts (from refs.ts). change realtime database connection constant name to 'realtimeDatabase' (from 'database')

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,4 +1,5 @@
 import { initializeApp } from 'firebase/app';
+import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyAh-tV-H0TbqxVHCSqMPdBLwQivTl_1u5I',
@@ -10,3 +11,5 @@ const firebaseConfig = {
 };
 
 export const app = initializeApp(firebaseConfig);
+
+export const realtimeDatabase = getDatabase(app);
