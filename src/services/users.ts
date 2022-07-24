02e5c50be82455e267c7eed5cf1ed8166794d76d
Gabor Makeev <gabor.makejev@gmail.com>
2022-07-24 20:19:15 +0200

    feat: implement user profile data writing and reading with firestore. create getUserDocRef function that returns the reference for the document of a specific user in the 'users' collection (refs.ts). remove usersRef firebase database reference, getUserPropertiesByEmail and getUserPropertyByEmailAndPropertyName functions (that return firebase database reference) (refs.ts). recreate the addUser function so that it works with firestore and creates a new document in the 'users' collection and names it after the user's email (users.ts). remove createFirebaseEmail function (users.ts). recreate data listening in Profile.tsx to work with firestore. recreate toggleIsPublic function in Profile.tsx to work with firestore

--- src/services/users.ts
+++ src/services/users.ts
@@ -1,21 +1,9 @@
-import { get, set } from 'firebase/database';
-import { usersRef } from 'src/services/refs';
-
-export const createFirebaseEmail = (email: string) => {
-  return email.replace(/\./g, '__DOT__');
-};
+import { getUserDocRef } from 'src/services/refs';
+import { setDoc, Timestamp } from 'firebase/firestore';
 
 export const addUser = async (userEmail: string) => {
-  const getUsersRequest = await get(usersRef);
-  const users = await getUsersRequest.val();
-
-  const prevUsers = users ? users : {};
-  const nextUsers = Object.assign(prevUsers, {
-    [createFirebaseEmail(userEmail)]: {
-      isPublic: false,
-      createdAt: Date.now(),
-    },
+  await setDoc(getUserDocRef(userEmail), {
+    createdAt: Timestamp.now(),
+    isPublic: false,
   });
-
-  await set(usersRef, nextUsers);
 };
