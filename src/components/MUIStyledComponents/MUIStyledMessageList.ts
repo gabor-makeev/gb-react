d9e0f4a7e0bb75738aad3d4f762362d63b3e1059
Gabor Makeev <gabor.makejev@gmail.com>
2022-08-24 16:39:26 +0200

    feat: make MessagesWindow take all the width and height available in mobile view; add spacing between messages in MessageList; update MessageList test snapshot

--- src/components/MUIStyledComponents/MUIStyledMessageList.ts
+++ src/components/MUIStyledComponents/MUIStyledMessageList.ts
@@ -4,5 +4,8 @@ import { styled } from '@mui/material';
 export const MUIStyledMessageList = styled(List)({
   width: '100%',
   maxHeight: '100%',
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '10px',
   overflowY: 'scroll',
 });
