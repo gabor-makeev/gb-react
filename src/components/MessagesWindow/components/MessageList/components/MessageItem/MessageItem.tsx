eac897ad736aff23f1479b5f52229118697d1605
Gabor Makeev <gabor.makejev@gmail.com>
2022-08-28 00:11:59 +0200

    feat: implement alignment of messages (MessageItem), make user messages show up on the right side of the message list and other user messages show up on the left side of the message list; implement alignment of the MessageItem component's content; update test snapshots according to changes

--- src/components/MessagesWindow/components/MessageList/components/MessageItem/MessageItem.tsx
+++ src/components/MessagesWindow/components/MessageList/components/MessageItem/MessageItem.tsx
@@ -2,7 +2,6 @@ import { FC } from 'react';
 import classNames from 'classnames';
 import style from './MessageItem.module.scss';
 import { Message } from 'src/default-types';
-import { MUIStyledMessageListItem } from 'components/MUIStyledComponents/MUIStyledMessageListItem';
 
 interface MessageItemProps {
   userEmail: string;
@@ -15,21 +14,21 @@ export const MessageItem: FC<MessageItemProps> = ({
   message,
   variant = 'medium',
 }) => {
-  const MessageListItemClasses = classNames(style[`message__type-${variant}`], {
-    [style['message__system-background']]: !message.userEmail,
-    [style['message__user-background']]: message.userEmail,
-    [style['message__other-user']]:
-      message.userEmail && message.userEmail !== userEmail,
-  });
+  const MessageListItemClasses = classNames(
+    style[`message__type-${variant}`],
+    style.message,
+    {
+      [style['message__system-background']]: !message.userEmail,
+      [style['message__user-background']]: message.userEmail,
+      [style['message__other-user']]:
+        message.userEmail && message.userEmail !== userEmail,
+    }
+  );
 
   return (
-    <MUIStyledMessageListItem
-      className={MessageListItemClasses}
-      disablePadding={true}
-      data-testid={'messageItem'}
-    >
+    <li className={MessageListItemClasses} data-testid={'messageItem'}>
       {message.body}
       <span className={style['message__author-sign']}>{message.userName}</span>
-    </MUIStyledMessageListItem>
+    </li>
   );
 };
