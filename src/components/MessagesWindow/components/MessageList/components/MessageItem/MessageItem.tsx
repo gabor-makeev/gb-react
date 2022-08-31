096e85e1afe1fc58777ef6073c4b4bef2ed8abe4
Gabor Makeev <gabor.makejev@gmail.com>
2022-08-31 23:00:02 +0200

    feat: implement message date string getting logic to MessageItem component

--- src/components/MessagesWindow/components/MessageList/components/MessageItem/MessageItem.tsx
+++ src/components/MessagesWindow/components/MessageList/components/MessageItem/MessageItem.tsx
@@ -9,6 +9,38 @@ interface MessageItemProps {
 }
 
 export const MessageItem: FC<MessageItemProps> = ({ userEmail, message }) => {
+  const date = new Date();
+  const messageDate = new Date(message.createdAt);
+
+  const isSameYear = date.getFullYear() === messageDate.getFullYear();
+  const isSameMonth = date.getMonth() === messageDate.getMonth();
+  const isSameDay = date.getDate() === messageDate.getDate();
+
+  const getDayOrMonthWithZero = (dayOrMonth: number): string => {
+    return dayOrMonth.toString().length === 1
+      ? `0${dayOrMonth}`
+      : dayOrMonth.toString();
+  };
+
+  const getMessageDateString = () => {
+    const messageDateDayWithZero = getDayOrMonthWithZero(messageDate.getDate());
+    const messageDateMonthWithZero = getDayOrMonthWithZero(
+      messageDate.getMonth() + 1
+    );
+
+    if (isSameYear) {
+      if (isSameDay) {
+        return 'Today';
+      } else if (isSameMonth && messageDate.getDate() === date.getDate() - 1) {
+        return 'Yesterday';
+      }
+
+      return `${messageDateDayWithZero}/${messageDateMonthWithZero}`;
+    }
+
+    return `${messageDateDayWithZero}/${messageDateMonthWithZero}/${messageDate.getFullYear()}`;
+  };
+
   const MessageListItemClasses = classNames(style.message, {
     [style['message__system-background']]: !message.userEmail,
     [style['message__auth-user']]:
