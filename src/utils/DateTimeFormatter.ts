9e018214c57168620984514a9a221d41fea40c4c
Gabor Makeev <gabor.makejev@gmail.com>
2022-09-01 23:43:45 +0200

    feat: create DateTimeFormatter class and move logic for formatted date getting into it (from MessageItem component); implement logic for identifying 'yesterday' date in the DateTimeFormatter class (including handling of previous month or year); use DateTimeFormatter in MessageItem component to get a formatted message date

---
+++ src/utils/DateTimeFormatter.ts
@@ -0,0 +1,55 @@
+export class DateTimeFormatter {
+  private readonly unixTime: number;
+  private readonly date: Date;
+  private readonly currentDate: Date;
+
+  constructor(unixTime: number) {
+    this.unixTime = unixTime;
+    this.date = new Date(this.unixTime);
+    this.currentDate = new Date();
+  }
+
+  get formattedDate() {
+    if (this.isDateToday()) {
+      return 'Today';
+    } else if (this.isDateYesterday()) {
+      return 'Yesterday';
+    }
+
+    return this.getDMYDate();
+  }
+
+  getDMYDate() {
+    const day = this.getDayOrMonthWithZero(this.date.getDate());
+    const month = this.getDayOrMonthWithZero(this.date.getMonth() + 1);
+    const year = this.date.getFullYear();
+
+    return `${day}/${month}/${year}`;
+  }
+
+  isDateToday() {
+    return (
+      this.date.getFullYear() === this.currentDate.getFullYear() &&
+      this.date.getMonth() === this.currentDate.getMonth() &&
+      this.date.getDate() === this.currentDate.getDate()
+    );
+  }
+
+  isDateYesterday() {
+    const today = new Date(this.currentDate);
+    const yesterday = new Date(today.setDate(today.getDate() - 1));
+
+    yesterday.setHours(
+      this.date.getHours(),
+      this.date.getMinutes(),
+      this.date.getSeconds(),
+      this.date.getMilliseconds()
+    );
+
+    return this.date.getTime() === yesterday.getTime();
+  }
+
+  getDayOrMonthWithZero(dayOrMonth: number) {
+    return dayOrMonth.toString().length === 1 ? `0${dayOrMonth}` : dayOrMonth;
+  }
+}
