257916ef07037409c247069df61a67c5300154dc
Gabor Makeev <gabor.makejev@gmail.com>
2022-09-06 19:46:08 +0200

    refactor: remove unused MUI styled components

--- src/components/MUIStyledComponents/MUIStyledChatSectionContainer.ts
+++
@@ -1,8 +0,0 @@
-import { Container } from '@mui/material';
-import { styled } from '@mui/material';
-
-export const MUIStyledChatSectionContainer = styled(Container)({
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'flex-end',
-});
--- src/components/MUIStyledComponents/MUIStyledMessageSectionContainer.ts
+++
@@ -1,7 +0,0 @@
-import { Container } from '@mui/material';
-import { styled } from '@mui/material';
-
-export const MUIStyledMessageSectionContainer = styled(Container)({
-  display: 'flex',
-  flexDirection: 'column',
-});
