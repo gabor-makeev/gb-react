7c55401c6bcc3960cbc189d1c60d689e8fa55409
Gabor Makeev <gabor.makejev@gmail.com>
2022-09-14 22:52:48 +0200

    feat: implement global Text component (with 2 styled variants BoldText and BoldHighlightedText) according to the designed layout

---
+++ src/components/global/Text/Text.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Text = styled.span`
+  font-family: 'Inter', sans-serif;
+  color: rgba(36, 76, 102, 0.7);
+  line-height: 19px;
+  letter-spacing: -0.011em;
+`;
